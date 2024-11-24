16e7332fed894aac5e4ce69fd0b78e581cb5ebc6
FahadAlHossain <fahadalhossain5@gmail.com>
2024-11-25 01:03:53 +0600

    catagory wise btn upload

--- index.js
+++ index.js
@@ -14,46 +14,57 @@ const displayCategory = (data) => {
     data.forEach(category => {
         // console.log(category.category)
         btnCategory.innerHTML += `
-            <button onclick="displayDetails()" class="btn btn-error text-white">${category.category}</button>
+            <button onclick="categoryBtn('${category.category_id}')" class="btn btn-error text-white">${category.category}</button>
         `;
     })
-    categoryBtn(data)
+    // categoryBtn(data)
 }
 
 
 const displayDetails = (data) => {
     const detailsCategory = document.getElementById('details-category');
-    console.log(data)
-    detailsCategory.innerHTML = `
-        <div class="card bg-base-100 w-96 shadow-xl">
-          <figure>
-            <img
-              src=
-              alt="Shoes" />
-          </figure>
-          <div class="card-body">
-            <h2 class="card-title">Shoes!</h2>
-            <p>If a dog chews shoes whose shoes does he choose?</p>
-            <div class="card-actions justify-end">
-              <button class="btn btn-primary">Buy Now</button>
+    // console.log(data)
+
+    detailsCategory.innerHTML = '';
+    data.forEach(items => {
+        // console.log(items.category_id)
+        detailsCategory.innerHTML += `
+            <div class="card bg-white text-black w-[98%] shadow-xl">
+              <figure>
+                <img class="w-96 h-72 rounded-xl"
+                  src=${items.thumbnail}
+                  alt="Shoes" />
+              </figure>
+              <div class="card-body">
+                    <div class="avatar">
+                      <div class="w-10 rounded-full">
+                         <img src=${items.authors[0].profile_picture} />
+                      </div>
+                        <h2 class="card-title pl-3">${items.title}</h2>
+                    </div>
+                    <div class="pl-14 text-slate-500">
+                        <p>${items.authors[0].profile_name} <span>${items.authors[0].verified}</span></p>
+                        <span>${items.others.views} views</span>
+                    </div>
+              </div>
             </div>
-          </div>
-        </div>
-    `
+        `
+    })
 }
 
-const categoryBtn = async (data) => {
-    // const ref = await fetch(`https://openapi.programming-hero.com/api/videos/category/`)
-    // const data = await ref.json();
-    // const load = data.data;
-    // console.log(load);
-    // displayDetails(load);
-    data.forEach(item => {
-        fetch(`https://openapi.programming-hero.com/api/videos/category/${item.category_id}`)
-        .then(res => res.json())
-        .then(data => console.log(data))
-    })
-   
+const categoryBtn = async (id) => {
+    const ref = await fetch(`https://openapi.programming-hero.com/api/videos/category/${id}`)
+    const data = await ref.json();
+    const load = data.data;
+    console.log(load);
+    displayDetails(load);
+    // data.forEach(item => {
+    //     fetch(`https://openapi.programming-hero.com/api/videos/category/${item.category_id}`)
+    //     .then(res => res.json())
+    //     .then(data => console.log(data))
 }
+
+categoryBtn(1000);
+   
 
 
